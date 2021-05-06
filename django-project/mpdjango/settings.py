2d4338aecb9a2f80530a3a0484692128b31bb8b1
mhead <mtrxhead@protonmail.com>
2021-05-06 23:11:17 +0530

    created api endpoints, authentication

--- django-project/mpdjango/settings.py
+++ django-project/mpdjango/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #############################
+    'rest_framework',
+    'rest_framework.authtoken',
+    'corsheaders',
+    #############################
     'nivedhanamapp.apps.NivedhanamappConfig'
 ]
 
@@ -49,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'mpdjango.urls'
@@ -126,3 +131,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+'DEFAULT_AUTHENTICATION_CLASSES': [
+'rest_framework.authentication.TokenAuthentication',
+],
+}
+
+CORS_ORIGIN_ALLOW_ALL = True
