7b3fc5f0b28f2130b118bacc8334690c53a15771
mhead <mtrxhead@protonmail.com>
2021-05-07 18:22:59 +0530

    added flutter project, configured dev server to server flutter app

--- django-project/nivedhanamapp/urls.py
+++ django-project/nivedhanamapp/urls.py
@@ -1,12 +1,24 @@
 from django.urls import include,path
 from rest_framework import routers
+from django.views.static import serve
+import os
 from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 router = routers.DefaultRouter()
 router.register(r'nivedhanams', views.NivedhanamViewSet)
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+FLUTTER_WEB_APP = os.path.join(BASE_DIR, 'flutter-project/build/web')
+
+def flutter_redirect(request, resource):
+    return serve(request, resource, FLUTTER_WEB_APP)
+
+
 urlpatterns =[
-    path('',include(router.urls)),
-    path('get-token/',obtain_auth_token)
+    path('api/',include(router.urls)),
+    path('get-token/',obtain_auth_token),
+
+    path('', lambda r: flutter_redirect(r, 'index.html')),
+    path('<path:resource>', flutter_redirect),
 ]
